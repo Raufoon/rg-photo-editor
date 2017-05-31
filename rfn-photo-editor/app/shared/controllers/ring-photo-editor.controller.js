11ef6385e8f9265a1cabbf6390846a04ee632e70
Raufoon <2012-012-020@students.cse.univdhaka.edu>
2017-06-01 02:23:29 +0600

    major

--- rfn-photo-editor/app/shared/controllers/ring-photo-editor.controller.js
+++ rfn-photo-editor/app/shared/controllers/ring-photo-editor.controller.js
@@ -1,13 +1,24 @@
 angular.module('ringid.shared')
-    .controller('ringPhotoEditorController', ringPhotoEditorController);
+.controller('ringPhotoEditorController', ringPhotoEditorController);
 
-ringPhotoEditorController.$inject = ['$scope'];
+ringPhotoEditorController.$inject = ['$scope', '$element'];
 
-function ringPhotoEditorController($scope) {
-    var isPortrait = false;
-    $scope.pebW = isPortrait? '55%' : '100%';
-    $scope.pebH = isPortrait? '100%' : '75%';
-    $scope.eopW = isPortrait? '45%': '100%';
-    $scope.eopH = isPortrait? '100%': '25%';
-    $scope.img = 'fake-server/portrait.jpg';
+function ringPhotoEditorController($scope, $element) {
+    var isPortrait=true,
+        canvasW = isPortrait? 55 : 71,
+        canvasH = isPortrait? 100 : 70,
+        filtersW = isPortrait? 16 : 71,
+        filtersH = isPortrait? 100 : 30,
+        optionsW = isPortrait? 20 : 20;
+
+    $scope.lW = (isPortrait? canvasW + filtersW : canvasW) + '%';
+    $scope.lH = '100%';
+
+    $scope.mW = optionsW + '%';
+
+    $scope.cW = (isPortrait? 80 : 100) + '%';
+    $scope.cH = (isPortrait? 100 : 80) + '%';
+
+    $scope.fW = (isPortrait? 20 : 100) + '%';
+    $scope.fH = (isPortrait? 100 : 20) + '%';
 }
--- rfn-photo-editor/app/shared/directives/ring-photo-editor.directive.js
+++
@@ -1,9 +0,0 @@
-angular.module('ringid.shared')
-.directive('ringPhotoEditor', ringPhotoEditorDirective);
-
-function ringPhotoEditorDirective() {
-    return {
-        restrict: 'A',
-        templateUrl: 'pages/ring-photo-editor.html',
-    }
-}
